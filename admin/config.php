e5c0189a2debbbe5e444f270dd1af45404c88566
MaxTecnology <maxsouza150@hotmail.com>
2024-02-27 11:09:10 -0300

    Feature: Configurações Tema Secoli

--- admin/config.php
+++ admin/config.php
@@ -1,27 +1,27 @@
 <?php
 
-add_action('admin_menu', 'meu_plugin_adicionar_menu_admin');
+add_action('admin_menu', 'config_theme_secoli');
 
-function meu_plugin_adicionar_menu_admin() {
+function config_theme_secoli() {
     add_menu_page(
-        'Config Themes',
-        'Config Themes', 
-        'manage_options', // Capacidade necessária para acessar esta página
-        'secoli-config-thema', // Slug da página
-        'secoli_config_thema', // Função que exibe o conteúdo da página
-        'dashicons-admin-customizer', // Ícone do menu (opcional)
-        99 // Posição do menu (opcional)
+        'Secoli',
+        'Secoli', 
+        'manage_options',
+        'secoli-config-thema',
+        'secoli_config_thema',
+        'dashicons-admin-customizer',
+        99
     );
 }
 
 function secoli_config_thema() {
     ?>
     <div class="wrap">
-        <h1>Configuração Smartlead</h1>
+        <h1>Configurações Tema Secoli</h1>
         <form method="post" action="options.php">
             <?php
-            settings_fields('secoli-config-theme-info'); // Opções do grupo que esta página irá manipular
-            do_settings_sections('secoli-config-thema'); // Slug da página para identificar as seções
+            settings_fields('secoli-config-theme-info');
+            do_settings_sections('secoli-config-thema');
             submit_button('Salvar Configurações');
             ?>
         </form>
@@ -32,98 +32,171 @@ function secoli_config_thema() {
 add_action('admin_init', 'secoli_register_config');
 
 function secoli_register_config() {
-    register_setting('secoli-config-theme-info', 'meu_plugin_opcoes');
+    register_setting('secoli-config-theme-info', 'secoli_theme_info');
 
     add_settings_section(
-        'meu_plugin_secao_db', // ID da seção
-        'Configurações do Banco de Dados', // Título da seção
+        'informacao', // ID da seção
+        'Informação', // Título da seção
         'secoli_title', // Callback para exibir a descrição da seção
         'secoli-config-thema' // Página em que esta seção deve ser exibida
     );
 
     add_settings_field(
-        'meu_plugin_campo_host', // ID do campo
-        'Host do Banco de Dados', // Título do campo
-        'meu_plugin_campo_host_callback', // Callback para renderizar o campo
+        'secoli_telefone_header', // ID do campo
+        'Telefone Header', // Título do campo
+        'secoli_telefone_header_callback', // Callback para renderizar o campo
         'secoli-config-thema', // Página em que este campo deve ser exibido
-        'meu_plugin_secao_db' // Seção em que este campo deve ser incluído
+        'informacao' // Seção em que este campo deve ser incluído
     );
 
     add_settings_field(
-        'meu_plugin_campo_nome_db',
-        'Nome do Banco de Dados',
-        'meu_plugin_campo_nome_db_callback',
-        'secoli-config-thema',
-        'meu_plugin_secao_db'
+        'secoli_telefone_rodape', // ID do campo
+        'Telefone Rodapé', // Título do campo
+        'secoli_telefone_rodape_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
     );
 
     add_settings_field(
-        'meu_plugin_campo_usuario',
-        'Usuário do Banco de Dados',
-        'meu_plugin_campo_usuario_callback',
-        'secoli-config-thema',
-        'meu_plugin_secao_db'
+        'secoli_telefone_whatsapp', // ID do campo
+        'Telefone Whatsapp', // Título do campo
+        'secoli_telefone_whatsapp_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
     );
 
     add_settings_field(
-        'meu_plugin_campo_senha',
-        'Senha do Banco de Dados',
-        'meu_plugin_campo_senha_callback',
-        'secoli-config-thema',
-        'meu_plugin_secao_db'
+        'secoli_email_header', // ID do campo
+        'E-mail Header', // Título do campo
+        'secoli_email_header_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
     );
 
     add_settings_field(
-        'meu_plugin_campo_porta',
-        'Porta do Banco de Dados',
-        'meu_plugin_campo_porta_callback',
-        'secoli-config-thema',
-        'meu_plugin_secao_db'
+        'secoli_email_rodape', // ID do campo
+        'E-mail Rodapé', // Título do campo
+        'secoli_email_rodape_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
     );
 
+    add_settings_field(
+        'secoli_email_project', // ID do campo
+        'E-mail para envio de projetos', // Título do campo
+        'secoli_email_project_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
+    );
 
-    // Repita add_settings_field() conforme necessário para outros campos como usuário, senha, etc.
+    add_settings_field(
+        'secoli_email_budget', // ID do campo
+        'E-mail para recebimento de orçamentos', // Título do campo
+        'secoli_email_budget_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
+    );
+
+    add_settings_field(
+        'secoli_email_newsletter', // ID do campo
+        'E-mail para cadastro de newsletter', // Título do campo
+        'secoli_email_budget_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
+    );
+
+    add_settings_field(
+        'secoli_facebook', // ID do campo
+        'Facebbok', // Título do campo
+        'secoli_facebook_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
+    );
+
+    add_settings_field(
+        'secoli_twitter', // ID do campo
+        'Twitter', // Título do campo
+        'secoli_twitter_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
+    );
+
+    add_settings_field(
+        'secoli_instagram', // ID do campo
+        'Instagram', // Título do campo
+        'secoli_instagram_callback', // Callback para renderizar o campo
+        'secoli-config-thema', // Página em que este campo deve ser exibido
+        'informacao' // Seção em que este campo deve ser incluído
+    );
+
 }
 
-// Callbacks para descrição da seção e campos
 function secoli_title() {
-    echo '<p>Insira as configurações de conexão ao seu banco de dados remoto.</p>';
+    echo '<p>Insira as informaçoes editáveis do tema</p>';
 }
 
-// function meu_plugin_campo_host_callback() {
-//     $opcoes = get_option('meu_plugin_opcoes');
-//     echo '<input type="text" name="meu_plugin_opcoes[host]" value="' . esc_attr($opcoes['host']) . '"/>';
-// }
+function secoli_telefone_header_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[telefone_header]" value="' . esc_attr($opcoes['telefone_header']) . '"/>';
+}
 
-// function meu_plugin_campo_nome_db_callback() {
-//     $opcoes = get_option('meu_plugin_opcoes');
-//     echo '<input type="text" id="nome_db" name="meu_plugin_opcoes[nome_db]" value="' . esc_attr($opcoes['nome_db'] ?? '') . '"/>';
-// }
+function secoli_telefone_rodape_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[telefone_rodape]" value="' . esc_attr($opcoes['telefone_rodape']) . '"/>';
+}
 
-// function meu_plugin_campo_usuario_callback() {
-//     $opcoes = get_option('meu_plugin_opcoes');
-//     echo '<input type="text" id="usuario" name="meu_plugin_opcoes[usuario]" value="' . esc_attr($opcoes['usuario'] ?? '') . '"/>';
-// }
+function secoli_telefone_whatsapp_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[telefone_whatsapp]" value="' . esc_attr($opcoes['telefone_whatsapp']) . '"/>';
+}
 
-// function meu_plugin_campo_senha_callback() {
-//     $opcoes = get_option('meu_plugin_opcoes');
-//     echo '<input type="password" id="senha" name="meu_plugin_opcoes[senha]" value="' . esc_attr($opcoes['senha'] ?? '') . '"/>';
-// }
+function secoli_email_header_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[email_header]" value="' . esc_attr($opcoes['email_header']) . '"/>';
+}
 
-// function meu_plugin_campo_porta_callback() {
-//     $opcoes = get_option('meu_plugin_opcoes');
-//     echo '<input type="text" id="porta" name="meu_plugin_opcoes[porta]" value="' . esc_attr($opcoes['porta'] ?? '') . '" placeholder="3306"/>';
-// }
+function secoli_email_rodape_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[email_rodape]" value="' . esc_attr($opcoes['email_rodape']) . '"/>';
+}
 
+function secoli_email_project_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[email_project]" value="' . esc_attr($opcoes['email_project']) . '"/>';
+}
+
+function secoli_email_budget_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[email_budget]" value="' . esc_attr($opcoes['email_budget']) . '"/>';
+}
+
+function secoli_facebook_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[facebook]" value="' . esc_attr($opcoes['facebook']) . '"/>';
+}
+
+function secoli_twitter_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[twitter]" value="' . esc_attr($opcoes['twitter']) . '"/>';
+}
+
+function secoli_instagram_callback() {
+    $opcoes = get_option('secoli_theme_info');
+    echo '<input type="text" name="secoli_theme_info[instagram]" value="' . esc_attr($opcoes['instagram']) . '"/>';
+}
 
 register_setting('secoli-config-theme-info', 'meu_plugin_opcoes', 'sanitize_opcoes_info');
 
 function sanitize_opcoes_info($opcoes) {
-    $opcoes['host'] = sanitize_text_field($opcoes['host']);
-    $opcoes['nome_db'] = sanitize_text_field($opcoes['nome_db']);
-    $opcoes['usuario'] = sanitize_text_field($opcoes['usuario']);
-    // A senha pode ser tratada com cuidado especial, talvez criptografada antes de salvar
-    $opcoes['senha'] = sanitize_text_field($opcoes['senha']);
-    $opcoes['porta'] = sanitize_text_field($opcoes['porta']);
+    $opcoes['telefone_header'] = sanitize_text_field($opcoes['telefone_header']);
+    $opcoes['telefone_rodape'] = sanitize_text_field($opcoes['telefone_rodape']);
+    $opcoes['telefone_whatsapp'] = sanitize_text_field($opcoes['telefone_whatsapp']);
+    $opcoes['email_header'] = sanitize_text_field($opcoes['email_header']);
+    $opcoes['email_rodape'] = sanitize_text_field($opcoes['email_rodape']);
+    $opcoes['email_project'] = sanitize_text_field($opcoes['email_project']);
+    $opcoes['email_budget'] = sanitize_text_field($opcoes['email_budget']);
+    $opcoes['facebook'] = sanitize_text_field($opcoes['facebook']);
+    $opcoes['twitter'] = sanitize_text_field($opcoes['twitter']);
+    $opcoes['instagram'] = sanitize_text_field($opcoes['instagram']);
     return $opcoes;
 }

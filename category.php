f172dd98066062a5d30eb711350bdbec22ab815b
MaxTecnology <maxsouza150@hotmail.com>
2024-03-13 11:52:01 -0300

    Feature: loop em categoy post

---
+++ category.php
@@ -0,0 +1,51 @@
+<?php get_header() ?>
+<div class="full about__banner">
+    <div class="container">
+        <div class="grid c-1 lg-c-2">
+            <div>
+                <h1 class="title">
+                    Secoli Brindes,
+                    conectando empresas, parceiros e pessoas.
+                </h1>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="full">
+    <div class="container">
+        <div class="long-space"></div>
+        <div class="grid c-1 lg-c-3">
+
+            <?php
+            if (have_posts()) {
+                while (have_posts()) : the_post();
+            ?>
+
+                    <div class="slide__blog__iten swiper-slide">
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full') ?>" alt="<?php the_title() ?>">
+                        <div class="space"></div>
+                        <h2 class="title">
+                            <?php the_title() ?>
+                        </h2>
+                        <p class="text">
+                            <?php the_excerpt() ?>
+                        </p>
+                        <div class="space"></div>
+                        <a href="<?php the_permalink() ?>" class="btn gradient-1">
+                            VEJA MAIS
+                        </a>
+                        <div class="space"></div>
+                    </div>
+
+            <?php
+                endwhile;
+            }
+            ?>
+
+        </div>
+        <div class="long-space"></div>
+    </div>
+</div>
+
+<?php get_footer() ?>
--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include __DIR__ . "/includ/factor-cunston-posts.php";
 include __DIR__ . "/includ/factor-meta-box.php";
@@ -14,29 +14,37 @@ include __DIR__ . "/admin/anuncio.php";
 add_theme_support('post-thumbnails');
 
 
-function __F($path){
-    echo esc_url( get_theme_file_uri( $path ) ); 
+function __F($path)
+{
+  echo esc_url(get_theme_file_uri($path));
 }
 
-function meu_tema_suporte_menu() {
-    register_nav_menus(
-      array(
-        'menu_principal' => 'Menu Principal',
-        'menu_rodape_institucioanal' => 'Institucioanal',
-        'menu_rodape_produtos' => 'Produtos',
-        'menu_rodape_links' => 'Links',
-        'menu_rodape_blog' => 'Blog',
-        'menu_rodape_informacoes' => 'Informações'
-      )
-    );
-  }
-  add_action('after_setup_theme', 'meu_tema_suporte_menu');
+function meu_tema_suporte_menu()
+{
+  register_nav_menus(
+    array(
+      'menu_principal' => 'Menu Principal',
+      'menu_rodape_institucioanal' => 'Institucioanal',
+      'menu_rodape_produtos' => 'Produtos',
+      'menu_rodape_links' => 'Links',
+      'menu_rodape_blog' => 'Blog',
+      'menu_rodape_informacoes' => 'Informações'
+    )
+  );
+}
+add_action('after_setup_theme', 'meu_tema_suporte_menu');
 
-  function print_menu($nome, $seletor) {
-    $locations = get_nav_menu_locations();
-    $menu_id = $locations[$nome];
-    $menu_items = wp_get_nav_menu_items($menu_id);
-    foreach ($menu_items as $menu_item) {
-        echo '<a href="' . $menu_item->url . '" class="'.$seletor.'" >' . $menu_item->title . '</a>';
-    }
+function print_menu($nome, $seletor)
+{
+  $locations = get_nav_menu_locations();
+  $menu_id = $locations[$nome];
+  $menu_items = wp_get_nav_menu_items($menu_id);
+  foreach ($menu_items as $menu_item) {
+    echo '<a href="' . $menu_item->url . '" class="' . $seletor . '" >' . $menu_item->title . '</a>';
   }
+}
+function custom_excerpt_length($length)
+{
+  return 20;
+}
+add_filter('excerpt_length', 'custom_excerpt_length', 999);

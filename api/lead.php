bad66cb07750a2ea09f3538608f25aec9e8597ae
MaxTecnology <maxsouza150@hotmail.com>
2024-03-20 10:50:41 -0300

    Fix: email

--- api/lead.php
+++ api/lead.php
@@ -20,8 +20,16 @@ add_action('rest_api_init', 'api_lead');
 function api_lead_fn($request)
 {
     $data = [
-        'nome'=>$request['nome'],
-        'email'=>$request['email']
+        'nome' => $request['nome'],
+        'email' => $request['email']
     ];
+
+    $to = 'maxsouza150@hotmail.com';
+    $subject = 'Novo lead';
+    $message = "Novo lead adicionado nome: " . $request['nome'] . " email: " . $request['email'];
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+
+    wp_mail($to, $subject, $message, $headers);
+
     return rest_ensure_response($data);
 }

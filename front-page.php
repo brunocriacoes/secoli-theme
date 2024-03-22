ede32485138375b36d8086532978c0e39cec958b
MaxTecnology <maxsouza150@hotmail.com>
2024-03-22 11:16:17 -0300

    Fix

--- front-page.php
+++ front-page.php
@@ -185,7 +185,7 @@ $opcoes = get_option('secoli_theme_info');
 			</label>
 			<span class="long-space"></span>
 			<div class="text__center">
-				<div class="news__alert js_lead_alert" hidden><i class="fa-solid fa-circle-info"></i><span>Alert</span></div>
+				<div class="news__alert js_lead_alert" ><i class="fa-solid fa-circle-info"></i><span>Alert</span></div>
 				<button class="btn gradient-1" type="submit">SOLICITAR AGORA</button>
 			</div>
 		</form>

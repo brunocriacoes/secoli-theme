200fbd7c91186a807d54ef826f5598c173ba9212
brunocriacoes <br.rafael@outlook.com>
2024-03-29 15:12:59 -0300

    fix

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,24 +1,4 @@
 
-new Swiper('.slider__blog', {
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 3000,
-    },
-    slidesPerView: 1,
-    spaceBetween: 40,
-    navigation: {
-        nextEl: '.slider__blog__next',
-        prevEl: '.slider__blog__pev',
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 40
-        }
-    }
-});
-
 // swiper
 // swiper-wrapper
 // swiper-slide

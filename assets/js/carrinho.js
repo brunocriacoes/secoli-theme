403f5994893baa094f5c41b48e80c4a0bac8b8d4
brunocriacoes <br.rafael@outlook.com>
2024-04-01 09:50:50 -0300

    fix cart

--- assets/js/carrinho.js
+++ assets/js/carrinho.js
@@ -88,7 +88,7 @@ class Cart {
         return `
         <label class="item__in__cart__input">
             <b>QTA-${i + 1}</b>
-            <input type="number" oninput="cart.atualizarQuantidade('${id}', '${i}', ${this.value})" name="quant[${id}][]"  min="1" value="${payload.value}">
+            <input type="number" oninput="cart.atualizarQuantidade('${id}', '${i}', '${this.value}')" name="quant[${id}][]"  min="1" value="${payload.value}">
         </label>
         `
     }

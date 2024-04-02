d32742fa29e1cf12e4342d0fd49d6b4ca0969094
MaxTecnology <maxsouza150@hotmail.com>
2024-04-02 11:25:35 -0300

    Fix: page home

--- front-page.php
+++ front-page.php
@@ -52,15 +52,11 @@ $opcoes = get_option('secoli_theme_info');
 
 <?php include __DIR__ . '/parts/destaque.php' ?>
 
-<div class="full idealize__bg">
+<div class="full idealize__bg title__idealize">
 	<div class="container">
-		<h3 class="title text-center">
+		<h3 class="title text-center ">
 			Procurando algo mais específico?
 		</h3>
-		<span class="long-space"></span>
-		<span class="long-space"></span>
-		<span class="long-space"></span>
-		<span class="long-space"></span>
 		<div class="grid c-1 lg-c-2">
 			<div></div>
 			<div>

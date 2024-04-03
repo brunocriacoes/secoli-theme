9253718a01a08dc927967cfa7ce6a7ec7a42b294
brunocriacoes <br.rafael@outlook.com>
2024-04-03 16:18:43 -0300

    css fix

--- admin/pagina_foto.php
+++ admin/pagina_foto.php
@@ -4,7 +4,7 @@ add_action('init', function() {
     $payload = array(
         array('nome' => 'secoli_page_foto_bloco1_title', 'tipo' => 'text'),
         array('nome' => 'secoli_page_foto_bloco1_desc', 'tipo' => 'textarea'),
-        array('nome' => 'secoli_page_foto_bloco1_img', 'tipo' => 'text'),
+        array('nome' => 'secoli_page_foto_bloco1_img', 'tipo' => 'file'),
 
     );
     adicionar_campos_personalizados($payload, 'post', 'secoli_page_foto_bloco1_metabox', 'Bloco Principal');
@@ -14,7 +14,7 @@ add_action('init', function() {
     $payload = array(
         array('nome' => 'secoli_page_foto_bloco2_title', 'tipo' => 'text'),
         array('nome' => 'secoli_page_foto_bloco2_desc', 'tipo' => 'textarea'),
-        array('nome' => 'secoli_page_foto_bloco2_img', 'tipo' => 'text'),
+        array('nome' => 'secoli_page_foto_bloco2_img', 'tipo' => 'file'),
 
     );
     adicionar_campos_personalizados($payload, 'post', 'secoli_page_foto_bloco2_metabox', 'Bloco de imagem esquerda');
@@ -24,7 +24,7 @@ add_action('init', function() {
     $payload = array(
         array('nome' => 'secoli_page_foto_bloco3_title', 'tipo' => 'text'),
         array('nome' => 'secoli_page_foto_bloco3_desc', 'tipo' => 'textarea'),
-        array('nome' => 'secoli_page_foto_bloco3_img', 'tipo' => 'text'),
+        array('nome' => 'secoli_page_foto_bloco3_img', 'tipo' => 'file'),
 
     );
     adicionar_campos_personalizados($payload, 'post', 'secoli_page_foto_bloco3_metabox', 'Bloco de imagem direita');

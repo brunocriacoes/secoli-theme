8a0630df26bc71e99930016c822deac4e5aa3529
brunocriacoes <br.rafael@outlook.com>
2024-04-03 10:12:11 -0300

    brindes fix

--- includ/router-magic.php
+++ includ/router-magic.php
@@ -1,7 +1,13 @@
 <?php
 
-function custom_rewrite_rule() {
-    add_rewrite_rule('^brindes/([^/]*)/?','index.php?pagename=brindes&produto=$matches[1]','top');
+function custom_rewrite_rule()
+{
+    add_rewrite_rule(
+        '^brindes/([^/]*)/?',
+        'index.php?rules_and_laws=$matches[1]',
+        'top'
+    );
+    flush_rewrite_rules();
 }
 add_action('init', 'custom_rewrite_rule', 10, 0);
 

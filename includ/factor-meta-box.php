d6c7dd3e4583fd3d067e06b4add0f44ae4568d24
brunocriacoes <br.rafael@outlook.com>
2024-04-03 16:21:36 -0300

    css fix

--- includ/factor-meta-box.php
+++ includ/factor-meta-box.php
@@ -26,30 +26,30 @@ function adicionar_campos_personalizados($campos, $post_type_slug, $metabox_id, 
                         <textarea id="<?php echo $nome_campo; ?>" name="<?php echo $nome_campo; ?>" rows="5" style="width:100%;"><?php echo esc_textarea($valor); ?></textarea>
                     <?php } ?>
                     <?php if ($tipo_campo == 'file') { ?>
-                        <input type="text" id="custom_featured_image" name="<?php echo $nome_campo; ?>" value="<?php echo esc_attr($valor); ?>" style="width:100%;" />
-                        <input type="button" id="custom_image_upload_button" class="button" value="Enviar Imagem" />
+                        <input type="text" id="<?php echo $nome_campo; ?>_input_img" name="<?php echo $nome_campo; ?>" value="<?php echo esc_attr($valor); ?>" style="width:100%;" />
+                        <input type="button" id="<?php echo $nome_campo; ?>_btn" class="button" value="Enviar Imagem" />
+                        <script>
+                            jQuery(document).ready(function($) {
+                                $('#<?php echo $nome_campo; ?>_btn').click(function(e) {
+                                    e.preventDefault();
+                                    var custom_uploader = wp.media({
+                                        title: 'Escolha uma imagem',
+                                        button: {
+                                            text: 'Selecionar imagem'
+                                        },
+                                        multiple: false
+                                    });
+                                    custom_uploader.on('select', function() {
+                                        var attachment = custom_uploader.state().get('selection').first().toJSON();
+                                        $('#<?php echo $nome_campo; ?>_input_img').val(attachment.url);
+                                    });
+                                    custom_uploader.open();
+                                });
+                            });
+                        </script>
                     <?php } ?>
                 </p>
 
-                <script>
-                    jQuery(document).ready(function($) {
-                        $('#custom_image_upload_button').click(function(e) {
-                            e.preventDefault();
-                            var custom_uploader = wp.media({
-                                title: 'Escolha uma imagem',
-                                button: {
-                                    text: 'Selecionar imagem'
-                                },
-                                multiple: false
-                            });
-                            custom_uploader.on('select', function() {
-                                var attachment = custom_uploader.state().get('selection').first().toJSON();
-                                $('#custom_featured_image').val(attachment.url);
-                            });
-                            custom_uploader.open();
-                        });
-                    });
-                </script>
 <?php
                 }
             },

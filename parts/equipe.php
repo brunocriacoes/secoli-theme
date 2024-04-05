e072bcf15af26eb473a9465919658b08ff7565dd
MaxTecnology <maxsouza150@hotmail.com>
2024-04-05 14:37:19 -0300

    Fix: CSS

--- parts/equipe.php
+++ parts/equipe.php
@@ -6,15 +6,15 @@ $equipes = get_posts(array(
 
 $loop = [];
 
-foreach($equipes as $equipe){
+foreach ($equipes as $equipe) {
     setup_postdata($equipe);
     $photo = get_the_post_thumbnail_url($equipe->ID, 'full');
-    $text = get_post_meta( $equipe->ID, 'secoli_equipe_text', true );
-    $name = get_post_meta( $equipe->ID, 'secoli_equipe_name', true );
-    $cargo = get_post_meta( $equipe->ID, 'secoli_equipe_cargo', true );
+    $text = get_post_meta($equipe->ID, 'secoli_equipe_text', true);
+    $name = get_post_meta($equipe->ID, 'secoli_equipe_name', true);
+    $cargo = get_post_meta($equipe->ID, 'secoli_equipe_cargo', true);
 
     wp_reset_postdata();
-    $loop[] =[
+    $loop[] = [
         "photo" => $photo,
         "text" => $text,
         "name" => $name,
@@ -25,31 +25,31 @@ foreach($equipes as $equipe){
 
 
 <div class="full profile__bg">
-		<div class="container">
-			<div class="long-space"></div>
-			<div class="card__profile">
-				<div class="card__profile__inner swiper-wrapper">
-                    <?php foreach($loop as $e){ ?>
-                        <div class="card__profile__iten swiper-slide">
-                            <img src="<?php echo $e['photo'] ?>" alt="avatar">
-                            <p>
-                                <span>
-                                    <i>
+    <div class="container">
+        <div class="long-space"></div>
+        <div class="card__profile">
+            <div class="card__profile__inner swiper-wrapper">
+                <?php foreach ($loop as $e) { ?>
+                    <div class="card__profile__iten swiper-slide">
+                        <!-- <img src="<?php echo $e['photo'] ?>" alt="avatar"> -->
+                        <p>
+                            <span>
+                                <i>
                                     <?php echo $e['text'] ?>
-                                    </i>
-                                </span>
-                            </p>
-                            <span class="space"></span>
-                            <p>
-                                <strong><?php echo $e['name'] ?></strong>
-                                <br>
-                                <i><?php echo $e['cargo'] ?></i>
-                            </p>
-                        </div>
-                    <?php } ?>
-				</div>
-			</div>
-			<div class="pagination__profile"></div>
-			<div class="long-space"></div>
-		</div>
-	</div>
+                                </i>
+                            </span>
+                        </p>
+                        <span class="space"></span>
+                        <p>
+                            <strong><?php echo $e['name'] ?></strong>
+                            <br>
+                            <i><?php echo $e['cargo'] ?></i>
+                        </p>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+        <div class="pagination__profile"></div>
+        <div class="long-space"></div>
+    </div>
+</div>

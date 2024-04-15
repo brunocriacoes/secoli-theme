17c117c3dec9d0e2ab154d6c73693fe0d70d133e
MaxTecnology <maxsouza150@hotmail.com>
2024-04-15 12:39:45 -0300

    Fix: url amigavel

--- page-produtos.php
+++ page-produtos.php
@@ -19,7 +19,7 @@ $categoriaAtual = 'Categoria atual';
 $slug = array_reverse(explode('/', $_SERVER['REQUEST_URI']))[0];
 var_dump($slug);
 foreach ($response as $c) {
-	if ($slug == $c['slug']) {
+	if ($id == $c['slug']) {
 		$categoriaAtual = $c['name'];
 	}
 }

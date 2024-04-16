9ab9a9b374adbaa9bcf0c1ad30915d4746570185
MaxTecnology <maxsouza150@hotmail.com>
2024-04-16 09:31:19 -0300

    Fix: links imagens

--- search.php
+++ search.php
@@ -87,7 +87,7 @@ $response_prod = array_values($response_prod);
                                 <span class="space"></span>
                                 <a class="btn gradient-1" onclick="return cart.add('<?php echo $produto['id'] ?>',
 												'<?php echo $produto['name'] ?>',
-												'https:\/\/app.secolibrindes.com.br\/<?php echo $produto['photos'][0]['path'] ?>', '<?php echo $produto['cod'] ?>', '', 'color', '<?php echo $_GET['cat'] ?>')" href="<?php __L('/') ?>carrinho">
+												'https:\/\/app.secolibrindes.com.br\/<?php echo $produto['photos'][0]['path'] ?>', '<?php echo $produto['cod'] ?>', '', 'color', '')" href="<?php __L('/') ?>carrinho">
                                     <i class="fa-solid fa-cart-plus"></i>
                                     <span>ORÇAR ESTE PRODUTO</span>
                                 </a>
